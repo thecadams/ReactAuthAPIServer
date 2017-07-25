e4d997c194a9ae118f30c92aa908cae6e9c161d8
Chris Adams <cadams@uber.com>
2017-07-24 17:13:43 -0700

    Initial revision

---
+++ controllers/authentication.js
@@ -0,0 +1,45 @@
+const User = require('../models/user')
+const jwt = require('jwt-simple')
+const config = require('../config')
+
+function tokenForUser(user) {
+  const timestamp = new Date().getTime()
+  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret)
+}
+
+exports.signin = function (req, res, next) {
+  // User has already had their email and password auth'd
+  // We just need to give them a token
+  res.send({ token: tokenForUser(req.user) })
+}
+
+exports.signup = function (req, res, next) {
+  // See if a user with the given email exists
+  const email = req.body.email
+  const password = req.body.password
+
+  if (!email || !password) {
+    return res.status(422).send('You must provide email and password')
+  }
+
+  // If a user with the email does exist, retutn error
+  User.findOne({ email: email }, function (err, existingUser) {
+    if (err) { return next(err) }
+
+    if (existingUser) {
+      return res.status(422).send({ error: 'Email is in use' })
+    }
+
+    // If user with email does not, create and save user record
+    const user = new User({
+      email: email,
+      password: password
+    })
+    user.save(function (err) {
+      if (err) { return next(err) }
+
+      // Respond to request indicating user was created
+      res.json({ token: tokenForUser(user) })
+    })
+  })
+}
